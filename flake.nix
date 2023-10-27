68fa7f175540c09d1b4106c8afd625f538642c50
Paul-Nicolas Madelaine <pnm@pnm.tf>
2023-10-27 09:32:51 +0200

    add package

--- flake.nix
+++ flake.nix
@@ -1,3 +1,14 @@
 {
-  outputs = {self}: {};
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+  };
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    packages.${system} = {inherit (pkgs) hello;};
+  };
 }
